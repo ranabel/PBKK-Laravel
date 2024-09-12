8d95a23f6421c90ae5024138187f3f5a499313b3
ranabel <nabilahatikarahma26@gmail.com>
2024-09-12 14:37:41 +0700

    week 1

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home', ['title' => 'Home Page']);
+});
+
+Route::get('/about', function () {
+    return view('about', ['name' => 'Nabilah Atika', 'title' => 'About']);
+});
+
+Route::get('/blog', function () {
+    return view('blog', ['title' => 'Blog']);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact']);
+});
+Route::get('/reports', function () {
+    return view('reports', ['title' => 'Reports']);
+});
+
